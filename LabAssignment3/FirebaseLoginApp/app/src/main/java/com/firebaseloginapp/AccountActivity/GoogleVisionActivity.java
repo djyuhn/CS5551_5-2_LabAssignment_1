0f296ea9302298560e747c667dc739299d421c34
DJ Yuhn <djyuhn@gmail.com>
2018-02-28 15:05:48 -0600

    Screenshots
    Screenshots of the application

--- LabAssignment3/FirebaseLoginApp/app/src/main/java/com/firebaseloginapp/AccountActivity/GoogleVisionActivity.java
+++ LabAssignment3/FirebaseLoginApp/app/src/main/java/com/firebaseloginapp/AccountActivity/GoogleVisionActivity.java
@@ -4,16 +4,12 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
 import android.widget.TextView;
 
 import com.firebaseloginapp.R;
