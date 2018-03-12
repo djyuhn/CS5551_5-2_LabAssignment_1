d32df2eabc46a9ce46d8ee5032240cc2745ef383
DJ Yuhn <djyuhn@gmail.com>
2018-03-11 22:17:00 -0500

    Lab4 Initial Commit
    Utilized code from ICP6 for login. Edited About Page and Contact Page.

---
+++ LabAssignment4/source/lab4/src/app/app.module.ts
@@ -0,0 +1,63 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+import { LoggedinPage } from '../pages/loggedin/loggedin';
+import { RegisterPage } from '../pages/register/register';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+const firebaseAuth = {
+    apiKey: "AIzaSyDa40yx7oy1d3c15op6nieX6VBtzvrNwkg",
+    authDomain: "labassignment4-cdfd2.firebaseapp.com",
+    databaseURL: "https://labassignment4-cdfd2.firebaseio.com",
+    projectId: "labassignment4-cdfd2",
+    storageBucket: "labassignment4-cdfd2.appspot.com",
+    messagingSenderId: "888327214903"
+  };
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    LoginPage,
+    RegisterPage,
+    LoggedinPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseAuth),
+    AngularFireAuthModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    LoginPage,
+    RegisterPage,
+    LoggedinPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
