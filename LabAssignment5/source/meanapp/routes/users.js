1256766ba4addb07d719b599c24941557fbc3dcb
DJ Yuhn <djyuhn@gmail.com>
2018-03-31 13:48:23 -0500

    User Registration
    Allow users to register.
    Create a route for user and a user model.
    Include screenshots

--- LabAssignment5/source/meanapp/routes/users.js
+++ LabAssignment5/source/meanapp/routes/users.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 // Register User
 router.post('/register', function(req, res, next) {
-    res.send('Register');
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, function(err, user) {
+        if(err) {
+            res.json({succcess: false, msg:'Failed to register the user'});
+        }
+        else {
+            res.json({succcess: true, msg:'User is registered'})
+        }
+    });
 });
 
 // Authenticate
