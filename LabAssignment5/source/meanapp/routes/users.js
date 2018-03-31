c85563b1370ab24ecfa989d854422c90fa9150f1
DJ Yuhn <djyuhn@gmail.com>
2018-03-31 12:45:39 -0500

    Remove validate
    Do not need validatation route.
    Change Register and Authenticate to post

--- LabAssignment5/source/meanapp/routes/users.js
+++ LabAssignment5/source/meanapp/routes/users.js
@@ -2,23 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 // Register User
-router.get('/register', function(req, res, next) {
+router.post('/register', function(req, res, next) {
     res.send('Register');
 });
 
 // Authenticate
-router.get('/authenticate', function(req, res, next) {
+router.post('/authenticate', function(req, res, next) {
     res.send('Authenticate');
 });
 
 // Profile
 router.get('/profile', function(req, res, next) {
     res.send('Profile');
-});
-
-// Validate
-router.get('/validate', function(req, res, next) {
-    res.send('Validate');
 });
 
 module.exports = router;
