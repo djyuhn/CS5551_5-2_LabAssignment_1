940f986df5b264ba61c2be40174fbec836217ada
DJ Yuhn <djyuhn@gmail.com>
2018-03-31 09:20:56 -0500

    Express Setup and Routes
    Establish connection with MongoDB using MLab.
    Setup backend Express server with dependencies.
    Create routes for user registration

---
+++ LabAssignment5/source/meanapp/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser'); //Middleware to parse incoming requests
+const cors = require('cors'); //Node module to allow access to any domain
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect to MLab Database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', function() {
+    console.log('Connected to database ' + config.database);
+});
+
+// On Error
+mongoose.connection.on('error', function(err) {
+    console.log('Database error: ' + err);
+});
+
+
+const app = express();
+
+const port = 3000;
+
+const users = require('./routes/users');
+
+// CORS Middleware 
+app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+// Index Route
+app.get('/', function(req, res) {
+    res.send('Invalid Endpoint');
+});
+
+// Start Express Server
+app.listen(port, function() {
+    console.log('Server started on port ' + port);
+});
